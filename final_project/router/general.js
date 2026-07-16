b76bb0df8ab45877c88580ca92a90e7d46c044b6
aln-f <144757715+aln-f@users.noreply.github.com>
2026-07-16 00:39:26 -0400

    Implement get books based on author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,9 +25,20 @@ public_users.get('/isbn/:isbn', function (req, res) {
 });
 
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+    const booksByAuthor = [];
+    const bookKeys = Object.keys(books);
+
+    bookKeys.forEach((key) => {
+        if (books[key].author == author) {
+            booksByAuthor.push(books[key]);
+        }
+    })
+    if (booksByAuthor.length > 0) {
+        return res.status(200).json(booksByAuthor);
+    }
+    return res.status(404).json({ message: "No books found by " + author });
 });
 
 // Get all books based on title
