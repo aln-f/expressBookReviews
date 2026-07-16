36545aeda71c823258cd53b0512c103ecf21cd9d
aln-f <144757715+aln-f@users.noreply.github.com>
2026-07-16 13:00:59 -0400

    Implement get book reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -59,9 +59,9 @@ public_users.get('/title/:title', function (req, res) {
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+    const isbn = req.params.isbn;    
+    return res.status(200).json(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
